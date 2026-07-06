59a3fc67ef0e2ef255d6943a19367516ae211709
Sk Nasrul <sk.nasrul.islam.06@gmail.com>
2026-07-06 20:41:37 +0530

    Delete functions/logout.js

--- functions/logout.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ success: true }), {
-    headers: {
-      "Content-Type": "application/json",
-      "Set-Cookie": "arcos_user=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax",
-    },
-  });
-}
