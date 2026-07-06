3407cfc964c0d6c6ec78dd08119852d0fcf035e9
Sk Nasrul <sk.nasrul.islam.06@gmail.com>
2026-07-06 20:40:53 +0530

    Delete functions/api directory

--- functions/api/logout.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost() {
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Set-Cookie": "session=; HttpOnly; Secure; Path=/; SameSite=Lax; Max-Age=0",
-    },
-  });
-}
