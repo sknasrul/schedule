4971726689bc0100c843a25f516c8db8d573e1f3
Sk Nasrul <prime.nasruls@gmail.com>
2026-07-23 21:13:30 +0530

    Update logins.js

--- functions/logins.js
+++ functions/logins.js
@@ -62,7 +62,7 @@ export async function onRequestPost(context) {
     );
 
     // Redirect after login
-    headers.set("Location", "/");
+    headers.set("Location", "/home");
 
     return new Response(null, {
         status: 302,
